d76d9183cdfce92af6d70451786b96be780db3e3
Arny <ucab.arny1@gmail.com>
2024-06-16 02:11:19 +0800

    Added - header and logo

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -5,14 +5,38 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
-  ImageBackground,
+  Image,
 } from "react-native";
-import { FontAwesome } from "@expo/vector-icons"; // or import from 'react-native-vector-icons/FontAwesome'
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function HomeScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.header}></View>
+      <View style={styles.headerContainer}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity
+            onPress={() => navigation.openDrawer()}
+            style={styles.iconButton}
+          >
+            <MaterialCommunityIcons name="menu" size={24} color="white" />
+          </TouchableOpacity>
+          <Image
+            source={require("../pictures/eventwise.png")}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Profile")}
+            style={styles.iconButton}
+          >
+            <MaterialCommunityIcons
+              name="account-circle"
+              size={24}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -20,6 +44,22 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    padding: 20,
+    backgroundColor: "black",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 5,
+    paddingTop: 10,
+    backgroundColor: "black", // Set the background color to black
+  },
+  iconButton: {
+    padding: 8,
+  },
+  logo: {
+    flex: 1,
+    height: 45,
+    marginHorizontal: 16,
   },
 });
